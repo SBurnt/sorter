503c67f65a513ee238a78027ef1cb23a8dc46745
SBurnt <zip2004zip@gmail.com>
2018-09-18 23:18:54 +0300

    task Sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.sorter = [];
+    this.comparator = (a, b) => a - b;
   }
 
   add(element) {
     // your implementation
+    this.sorter.push(element);
   }
 
   at(index) {
-    // your implementation
+    // your implementation+
+    return this.sorter[index];
   }
 
   get length() {
     // your implementation
+    return this.sorter.length;
   }
 
   toArray() {
     // your implementation
+    return this.sorter;
   }
 
   sort(indices) {
     // your implementation
+    let filterArr = this.sorter.filter((item, index) => {
+      return indices.includes(index);
+    });
+
+    filterArr.sort(this.comparator).reverse();
+
+    this.sorter = this.sorter.map((item, index) => {
+      return indices.includes(index) ? filterArr.pop() : item;
+    });
+
+    return this.sorter;
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.comparator = compareFunction;
   }
 }
 
